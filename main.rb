73d3fbeaefd78d3ecb985976b7e50dcb4e307909
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-21 13:46:07 -0600

    refractored

--- main.rb
+++ main.rb
@@ -1,2 +1,13 @@
 require './players'
-require './game_play'
+require './question'
+require './game'
+
+
+print "Enter first player name: "
+player_1 = Player.new(gets.chomp)
+print "Enter second player name: "
+player_2 = Player.new(gets.chomp)
+
+new_game = Game.new(player_1, player_2)
+new_game.play
+new_game.game_over
--- players.rb
+++ players.rb
@@ -1,23 +1,4 @@
-module Questions
-  
-  MAX_RANGE = 20
-  def question
-
-    number_1 = rand(MAX_RANGE)
-    number_2 = rand(MAX_RANGE)
-    print "What does #{number_1} plus #{number_2} equal? "
-    answer = gets.chomp
-    puts answer
-
-    answer.to_i == number_1 + number_2 ? true : return
-  end
-
-end
-
-
 class Player
-  
-  include Questions
 
   # You can change the number of lives for the game here
   LIVES = 3
